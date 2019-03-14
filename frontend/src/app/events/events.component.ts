701215258edbec8806238bbbfae58febede31953
Danilo Andrade <danilotandrade1518@gmail.com>
2019-03-14 01:22:31 -0300

    Alteração na versão do Bootstrap. Alteração no Typeahead.

--- frontend/src/app/events/events.component.ts
+++ frontend/src/app/events/events.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { Subject } from 'rxjs';
-import { debounceTime, tap, switchMap, retryWhen } from 'rxjs/operators';
+import { Subject, Subscription, of } from 'rxjs';
+import { debounceTime, tap, switchMap, retryWhen, filter } from 'rxjs/operators';
 import { EventsService } from './../events/events.service';
 import { EventResponse } from './../models/event-response.model';
 import { Event } from './../models/event.model';
@@ -12,15 +12,17 @@ import { EventResponseBuilder } from './../models/event-response.builder';
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
-export class EventsComponent implements OnInit {
+export class EventsComponent implements OnInit, OnDestroy {
   currentPage = 1;
   currentSearch = '';
-  eventResponse: EventResponse;
+  eventResponse: EventResponse = EventResponseBuilder.build();
   events: Event[] = [];
   loading = false;
-  // autoCompleteLoading = false;
   hasError = false;
-  selectedEvent;
+  selectedEvent: Event = new Event('', '');
+
+  searchStringSubscription: Subscription;
+  getEventsSubscription: Subscription;
 
   private searchString: Subject<string> = new Subject<string>();
 
@@ -30,52 +32,58 @@ export class EventsComponent implements OnInit {
     this.getEvents();
   }
 
+  ngOnDestroy() {
+    this.searchStringSubscription.unsubscribe();
+    this.getEventsSubscription.unsubscribe();
+  }
+
   handleSearchChange(event) {
     const keyCode = event.which;
     const alphaNumericKeyCodes = Array.from(Array.from(Array(Math.ceil((91 - 48) / 1)).keys()), x => 48 + x * 1);
     const validKeys = [8, 46, ...alphaNumericKeyCodes];
 
     if ( validKeys.includes(keyCode) ) {
-      const value = event.target.value;
-
       this.resetResponse();
 
-      if (value && value.length >= 2) {
-        this.loading = true;
-        this.searchString.next(event.target.value);
-      }
+      this.searchString.next(event.target.value);
     }
   }
 
   getEvents() {
     const getEventsFromApi = search => this.eventsService.getEvents(search, this.currentPage);
+    const checkInitLoading = search => this.loading = (search.length >= 2);
+    const checkStopLoadingAndFilter = search => {
+      this.loading = search.length >= 2;
+      return search && search.length >= 2;
+    };
+    const handleErrors = errors => errors.pipe(
+      tap(val => {
+        this.hasError = true;
+        this.resetResponse();
+      }),
+    );
+    const handleSuccess = eventResponse => {
+      this.hasError = false;
+      this.eventResponse = eventResponse;
+      this.events = this.eventResponse.events;
+      this.loading = false;
+    };
 
-    this.searchString.pipe(
+    this.searchStringSubscription = this.searchString.pipe(
+      tap(checkInitLoading),
       debounceTime(800),
+      filter(checkStopLoadingAndFilter),
       tap(search => this.currentSearch = search),
       switchMap(getEventsFromApi),
-      retryWhen(errors =>
-        errors.pipe(
-          tap(val => {
-            this.hasError = true;
-            this.resetResponse();
-          }),
-        )
-      ),
-      tap(res => {
-        this.hasError = false;
-        this.eventResponse = res;
-        this.events = this.eventResponse.events;
-        this.loading = false;
-      }),
+      retryWhen(handleErrors),
+      tap(handleSuccess),
     ).subscribe();
   }
 
   onScrollAtBottom() {
-    console.log('SCROLL!');
     this.loading = true;
 
-    this.eventsService.getEvents(this.currentSearch, this.currentPage + 1).pipe(
+    this.getEventsSubscription = this.eventsService.getEvents(this.currentSearch, this.currentPage + 1).pipe(
       tap(eventResponse => {
         if (eventResponse.pagination.currentEvents > 0) {
           this.currentPage++;
